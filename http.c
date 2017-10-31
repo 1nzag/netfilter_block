82b1a19894b5d2aaca264f3241e0e3e732ff84e4
1nzag <wkdwns420@gmail.com>
2017-10-31 04:57:02 -0700

    Add Filetering

--- http.c
+++ http.c
@@ -1,32 +1,57 @@
 #include <stdio.h>
 
-int is_HTTP(u_char* packet)
+int is_HTTP(unsigned char* data)
 {
 	int flag = 0;
 	
-	if(!strncmp("GET", packet, 3))
+	if(!strncmp("GET", data, 3))
 	{
 		flag = 1;
 	}
-	if(!strncmp("HOST",packet,4))
+	if(!strncmp("HOST",data,4))
 	{
 		flag = 1;
 	}
-	if(!strncmp("HEAD",packet,4))
+	if(!strncmp("HEAD",data,4))
 	{
 		flag = 1;
 	}
-	if(!strncmp("PUT",packet,4))
+	if(!strncmp("PUT",data,4))
 	{
 		flag = 1;
 	}
-	if(!strncmp("DELETE",packet,4))
+	if(!strncmp("DELETE",data,4))
 	{
 		flag = 1;
 	}
-	if(!strncmp("OPTIONS",packet,4))
+	if(!strncmp("OPTIONS",data,4))
 	{
 		flag = 1;
 	}
 	return flag;
 }
+
+
+int filter(struct nfq_data *tb)
+{
+	struct ip *ip_header; 
+	struct tcphdr *tcp_header;
+	unsigned char* data;
+	int size;
+	size = nfq_get_payload(tb, &data);
+	ip_header = data;
+	if(ip_header->ip_p != 6) //is not tcp
+	{
+		return 0;
+	}
+	tcp_header = ip_header + ((ip_header -> ip_hl) * 4); // ip_header_size
+	data = tcp_header + ((tcp_header -> th_off) * 4); //tcp_header_size
+	if(!is_HTTP(data))
+	{
+		return 0;
+	}
+	return 1;
+}
+
+	
+	
